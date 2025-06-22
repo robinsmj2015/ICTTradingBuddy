078381b576c2017b837d72a1c807d9dd7168167f
robinsmj2015 <robinsmj2015@comcast.net>
2025-06-22 08:27:38 -0400

    checking for page bloating still

--- Processor.py
+++ Processor.py
@@ -21,9 +21,9 @@ def process_symbol(buddy):
     # Get tick data
 
     # synthetic
-    #buddy.last_tick = make_synthetic_data(buddy.last_tick)
+    buddy.last_tick = make_synthetic_data(buddy.last_tick)
 
-    buddy.last_tick = get_last_tick()
+    #buddy.last_tick = get_last_tick()
 
     # Update each candle timeframe with the tick
     for candle in buddy.candles.values():
